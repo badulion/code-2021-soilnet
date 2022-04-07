049bb822a1974f3710153362cb9655075aff079e
Florian <frudolph@posteo.de>
2022-04-07 16:32:27 +0200

    patchDS aktualisiert

--- dataset/dataloader/patchDS.py
+++ dataset/dataloader/patchDS.py
@@ -6,12 +6,17 @@ from torch.utils.data.dataset import Dataset
 from utils import generate_patch
 from hydra.utils import get_original_cwd
 from pathlib import Path
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
 
 
-class PatchDataModule(Dataset):
+class PatchData(Dataset):
     def __init__(self,
                 path_lab,
                 path_unlab,
+                mode,
+                fold,
+                num_splits,
                 n,
                 deviation_to_shrink_df,
                 deviation_for_perfect_hit1,
@@ -22,6 +27,9 @@ class PatchDataModule(Dataset):
                  ):
 
         self.n = n
+        self.fold = fold
+        self.mode = mode
+        self.num_splits = num_splits
         self.path_lab = path_lab
         self.path_unlab = path_unlab
         self.deviation_to_shrink_df = deviation_to_shrink_df
@@ -38,7 +46,7 @@ class PatchDataModule(Dataset):
 
         # generate path for patch
         filestart = "../data/Feb_2022/"
-        fileend = "patch_" + str(self.n) + "_" + str(self.all_features)
+        fileend = "patch_" + str(self.n) + "_" + str(self.all_features) + self.mode
         file = filestart + fileend + ".npy"
         self.patch_path = Path(os.path.join(get_original_cwd(), file))
 
@@ -47,11 +55,9 @@ class PatchDataModule(Dataset):
             self.generate_patch()
         self.patch = np.load(self.patch_path)
 
-    # Datamodule erstellen - labaledDS orientieren
-
     def generate_patch(self):
         # prepare data
-        self.data_lab = pd.read_csv(self.path_lab)
+        self.data_lab = self._setup_data(self.fold)
         self.data_unlab = pd.read_csv(self.path_unlab)
         # prepare features
         self.data_lab = self.data_lab[self.all_features]
@@ -68,6 +74,7 @@ class PatchDataModule(Dataset):
                                                        )
 
         result = patchgenerator.generate_patch()
+        self.patch = result
         np.save(self.patch_path, result)
         return
 
@@ -76,6 +83,134 @@ class PatchDataModule(Dataset):
 
     def __len__(self):
         return len(self.data_lab.index)
+
+    def _split_data(self, df, strategy="location"):
+        assert strategy in ["location", "sample"]
+        if strategy == "location":
+            location_columns = self.features_metrical[:2]
+            df.set_index(location_columns, inplace=True)
+            locations = df.index.unique()
+            location_splits = np.array_split(locations, self.num_splits)
+            data_splits = [df.loc[fold].reset_index() for fold in location_splits]
+        else:
+            data_splits = np.array_split(df, self.num_splits)
+
+        return data_splits
+
+    def _setup_data(self, fold):
+        data = pd.read_csv(self.path_lab)
+        data = data.sample(frac=1, random_state=42)
+        data_splits = self._split_data(data)
+
+        test_fold = fold
+        val_fold = (fold + 1) % 10
+        train_folds = [i for i in range(10) if i != test_fold and i != val_fold]
+
+        if self.mode == "test":
+            data = data_splits[test_fold]
+        elif self.mode == "val":
+            data = data_splits[val_fold]
+        else:
+            data = pd.concat([data_splits[i] for i in train_folds])
+        return data
+
+class PatchDataModule(pl.LightningDataModule):
+    def __init__(self,
+                path_lab,
+                path_unlab,
+                n,
+                fold: int = 0,
+                mode="val",
+                features_metrical=["x", "y"],
+                features_categorical=None,
+                num_splits=10,
+                batch_size=16,
+                num_workers=1,
+                deviation_to_shrink_df=100,
+                deviation_for_perfect_hit1=50,
+                deviation_for_perfect_hit2=25,
+                deviation_between_two_points=50):
+        super(PatchDataModule, self).__init__()
+
+        # save parameters
+        self.n = n
+        self.mode = mode
+        self.path_lab = path_lab
+        self.path_unlab = path_unlab
+        self.fold = fold
+        self.features_metrical = features_metrical
+        self.features_categorical = features_categorical
+        self.num_splits = num_splits
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.deviation_to_shrink_df = deviation_to_shrink_df
+        self.deviation_for_perfect_hit1 = deviation_for_perfect_hit1
+        self.deviation_for_perfect_hit2 = deviation_for_perfect_hit2
+        self.deviation_between_two_points = deviation_between_two_points
+
+
+        #internal setup
+        self._is_setup = False
+
+    def setup(self, stage=None):
+        self.train = PatchData(path_lab=self.path_lab,
+                                path_unlab=self.path_lab,
+                                mode="train",
+                                fold=self.fold,
+                                num_splits=self.num_splits,
+                                n = self.n,
+                                deviation_to_shrink_df=self.deviation_to_shrink_df,
+                                deviation_for_perfect_hit1=self.deviation_for_perfect_hit1,
+                                deviation_for_perfect_hit2=self.deviation_for_perfect_hit2,
+                                deviation_between_two_points=self.deviation_between_two_points,
+                                features_metrical=self.features_metrical,
+                                features_categorical=self.features_categorical)
+
+        self.val = PatchData(path_lab=self.path_lab,
+                                path_unlab=self.path_lab,
+                                mode=self.mode,
+                                fold=self.fold,
+                                num_splits=self.num_splits,
+                                n = self.n,
+                                deviation_to_shrink_df=self.deviation_to_shrink_df,
+                                deviation_for_perfect_hit1=self.deviation_for_perfect_hit1,
+                                deviation_for_perfect_hit2=self.deviation_for_perfect_hit2,
+                                deviation_between_two_points=self.deviation_between_two_points,
+                                features_metrical=self.features_metrical,
+                                features_categorical=self.features_categorical)  # 86450 85208 86174
+        self.test = PatchData(path_lab=self.path_lab,
+                                path_unlab=self.path_lab,
+                                mode=self.mode,
+                                fold=self.fold,
+                                num_splits=self.num_splits,
+                                n = self.n,
+                                deviation_to_shrink_df=self.deviation_to_shrink_df,
+                                deviation_for_perfect_hit1=self.deviation_for_perfect_hit1,
+                                deviation_for_perfect_hit2=self.deviation_for_perfect_hit2,
+                                deviation_between_two_points=self.deviation_between_two_points,
+                                features_metrical=self.features_metrical,
+                                features_categorical=self.features_categorical)
+
+        # normalize the data
+        # self.scaler = StandardScaler()
+        # self.train.normalize(self.scaler, fit=True)
+        # self.val.normalize(self.scaler, fit=False)
+        # self.test.normalize(self.scaler, fit=False)
+
+        self._is_setup = True
+
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def predict_dataloader(self):
+        return DataLoader(self.test, batch_size=self.batch_size, num_workers=self.num_workers)
+
 
 @hydra.main(config_path='../../conf', config_name='config')
 def test(cfg):
@@ -89,10 +224,11 @@ def test(cfg):
                             deviation_for_perfect_hit2=cfg.patch.parameters.deviation_for_perfect_hit2,
                             deviation_between_two_points=cfg.patch.parameters.deviation_between_two_points,
                             features_metrical = cfg.vars.features_metrical,
-                            features_categorical = cfg.vars.features_categorical
+                            features_categorical = cfg.vars.features_categorical,
+                            mode="val"
                               )
-    x = patchDS.__getitem__(10)
-    c = 0
+    patchDS.setup()
+    x = 0
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
